248fa5435d65af24114bffe92e0433d3d538bc78
amyhskwon <amyhskwon@gmail.com>
2022-11-01 08:44:31 -0700

    commit tree 8ebb31eb751ad49387f7a27dba39448597ea9553

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+let num1 = 15.5
+let num2 = 4
+const multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10);
+
+const num3 = 25
+const num4 = 7
+const mod = num3 % num4;
+
+const max = Math.max(3,2,14,1,6,9,11,4,20,5,15);
